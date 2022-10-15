58256c0b1ebbc335d31e9940632465e951128b66
SlikeDollar <mibonbon73@gmail.com>
2022-10-16 01:11:46 +0200

    feat: extended repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
@@ -14,12 +16,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
- */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ *options is an object of options, that contains properties:
+repeatTimes sets the number of repetitions of the str;
+separator is a string separating repetitions of the str;
+addition is an additional string that will be added to each repetition of the str;
+additionRepeatTimes sets the number of repetitions of the addition;
+*additionSeparator is a string separating repetitions of the addition.
+*/
+function repeater(str, options) {
+    
+  function additionStr(addition = options.addition, repTimes = options.additionRepeatTimes, separator = options.additionSeparator) {
+    if (options.hasOwnProperty('addition')) {
+      addition = String(addition);
+
+      if (repTimes) {
+        let tempArr = [];
+
+        for (let i = 0; i < repTimes; i++) {
+          tempArr.push(addition);
+        }
+
+        if (separator) {
+          addition = tempArr.join(`${separator}`);
+        } else {
+          addition = tempArr.join(`|`);
+        }
+      }
+
+      return addition;
+    }
+    return '';
+  }
+
+
+  function string(string = str, repTimes = options.repeatTimes, separator = options.separator) {
+      string = String(string) + additionStr();
+
+      if (repTimes) {
+        let tempArr = [];
+
+        for (let i = 0; i < repTimes; i++) {
+          tempArr.push(string);
+        }
+
+        if (options.hasOwnProperty('separator')) {
+          string = tempArr.join(`${separator}`);
+        } else {
+          string = tempArr.join(`+`);
+        }
+      }
+
+      return string;
+  }
+
+  return string();
 }
 
+ console.log( repeater('STRING', { repeatTimes: 3, separator: '**', 
+ addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
+
+// str => addition chain + adding => repeatTimes (pushing into an array) => separator (join method)
+// addition => additionRepeatTimes (pushing into an array) => additionSeparator (join method)
 module.exports = {
   repeater
 };
