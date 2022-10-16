5b86730a537697c3d30347ff1e58ced5e88752e5
SlikeDollar <mibonbon73@gmail.com>
2022-10-16 23:26:44 +0200

    feat: encode line, get-email-domain DONE

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,18 +1,27 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Given a string, return its encoding version.
+ * Given a str, return its encoding version.
  *
- * @param {String} str
- * @return {String}
+ * @param {str} str
+ * @return {str}
  *
  * @example
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+    let x = 1;    
+    let res = '';
+    for(let i=0; i < str.length; i++){
+        if(str[i] === str[i+1]){
+            x += 1;
+        } else {
+            res += x+str[i];
+            x = 1;
+        }
+    }
+    return res.replace(/1/g, '');
 }
 
 module.exports = {
